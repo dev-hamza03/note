a7e5556ad05c65e537cc40bfe357ae8d1faad728
Soham Bangar <sohambangar188@gmail.com>
2026-02-08 23:53:29 +0530

    path fixed

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -7,7 +7,7 @@ const path = require('path')
 const app = express()
 app.use(express.json())
 app.use(cors())
-app.use(express.static("./public"))
+app.use(express.static(path.join(__dirname, "../public")))
 
 /**
  * - POST /api/notes
