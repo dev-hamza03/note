de233d81e6ad49b61fd2e2fa81e25aa230b94272
Hamza <dev.hamza03@gmail.com>
2026-02-10 05:06:35 +0530

    correct wildcard

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -75,7 +75,7 @@ app.patch("/api/notes/:id", async (req, res) => {
   });
 });
 
-app.get('*name', (req, res) => {
+app.use('*name', (req, res) => {
   res.sendFile(path.join(__dirname, "..", "public/index.html"));
 });
 
